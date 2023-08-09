65fa1fea858b76a865ded2b0dbb7e3e7ace6b427
Kai <kai.traupel@student.ap.be>
2023-08-09 12:46:26 +0200

    verkregen data in leesbare json veranderen

--- trainboard/trainboard.py
+++ trainboard/trainboard.py
@@ -1,14 +1,20 @@
 import http.client
 import json
+import turtle
 from datetime import datetime
 
 # url voor de data die nodig is voor het liveboard van Antwerpen-Centraal
 url = "/liveboard/?station=Antwerpen-Centraal&arrdep=departure&lang=nl&format=json&alerts=false"
 
+# verbinding maken met iRail API
 conn = http.client.HTTPSConnection("api.irail.be")
 conn.request("GET", "/liveboard/?station=Antwerpen-Centraal&arrdep=departure&lang=nl&format=json&alerts=false")
 res = conn.getresponse()
 data = res.read().decode("utf-8")
+conn.close()
+
+# data in meer leesbare json vorm steken
+data = json.loads(data)
 
 
 
