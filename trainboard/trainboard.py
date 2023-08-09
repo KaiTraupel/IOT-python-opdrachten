5cb7b406127d19b2f2bfdc520a0fac5f769d80f1
Kai <kai.traupel@student.ap.be>
2023-08-09 12:43:34 +0200

    eerste verbinding maken met iRail API

--- trainboard/trainboard.py
+++ trainboard/trainboard.py
@@ -1,23 +1,14 @@
-import http.client as website
+import http.client
 import json
 from datetime import datetime
 
+# url voor de data die nodig is voor het liveboard van Antwerpen-Centraal
 url = "/liveboard/?station=Antwerpen-Centraal&arrdep=departure&lang=nl&format=json&alerts=false"
 
-conn = website.HTTPSConnection("api.irail.be")
-payload = ""
-headers = {}
-
-conn.request("GET", url, payload, headers)
+conn = http.client.HTTPSConnection("api.irail.be")
+conn.request("GET", "/liveboard/?station=Antwerpen-Centraal&arrdep=departure&lang=nl&format=json&alerts=false")
 res = conn.getresponse()
-
-data = json.loads(res.read())
-
-# print(data)
-
-ts = int('1691517687')
-
-print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+data = res.read().decode("utf-8")
 
 
 
